88d6a544c40bd61d5a1cc1e8fba2bc2f4a8f6404
Andhikaptr <79065496+Andhikapratamap@users.noreply.github.com>
2022-05-30 16:48:59 +0700

    Add files via upload

---
+++ 1.Pendahuluan/script.js
@@ -0,0 +1,9 @@
+ document.writeln("ini ditulis di file Javascript terpisah")
+ 
+ // objek document mempunyai 2 metode penulisan yaitu 
+    document.write("Tanpa ganti baris") //menuliskan tanpa ganti baris
+    
+    //dan 
+    
+    document.writeln("Untuk membuat baris baru") //menuliskan dibaris baru
+    
